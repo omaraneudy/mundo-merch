06e5883e36b036c7b5c8f6317d637072bd033131
Omar <omaraneudye@gmail.com>
2021-09-04 21:27:46 -0400

    creado interfaz de login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms';
+import validator from 'validator';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
